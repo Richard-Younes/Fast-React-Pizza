cfe63e687238a5f57f058187f17609512b58c6f2
Richard Younes <richardyounes01@gmail.com>
2024-06-08 14:23:43 +0300

    Initial commit
    In this commit we finished some of the JSX and tailwind implementation

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold md:block">Richard</div>;
+}
+
+export default Username;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+const config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+
+    extend: {
+      height: {
+        screen: "100dvh",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
